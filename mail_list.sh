280b7c63433e2caf8a881d58e0c185f5a39e7200
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2014-07-31 19:46:59 -0300

    * mail_list.sh: regexp update

--- mail_list.sh
+++ mail_list.sh
@@ -12,10 +12,10 @@ while read m; do
     mailx -R -H -f +$m |
     grep -H --label=$m -e '^N '
 done |
-sed -r 's/^\s+//;s/\bRe:\s//g;s/\s+/ /g' |
+sed -r 's/^\s+//;s/\b(Res?|Fwd):\s//gi;s/\s+/ /g' |
 sort |
 uniq -c |
-sed -r 's/^\s+//;s/\s/\t/;s/:/:\t/' |
+sed -r 's/^\s+//;s/\s/\t/;s/:/:\t/;/^1\s/d' |
 sort -t$'\t' -k2,2 -k1,1nr |
 column -s$'\t' -t |
 less
